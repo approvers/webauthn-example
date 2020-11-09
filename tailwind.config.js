8a87134fc73e667ed04a5953a656d2e719ad58e8
Naoki Ikeguchi <root@siketyan.dev>
2020-11-09 10:35:34 +0900

    🔧 Configure Tailwind with PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  variants: {
+  },
+  plugins: [
+  ],
+}
